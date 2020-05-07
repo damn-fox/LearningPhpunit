4f10534a71fd6fdda4ed3f485888b2412247aefc
damn-fox <damnfox@zohomail.eu>
2020-05-07 11:24:49 +0200

    first commit

---
+++ tests/MoneyTest/MoneyTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class MoneyTest extends TestCase
+{
+    public function testCreateValidInstance(): void
+    {
+        $this->assertInstanceOf(
+            Money::class,
+            Money::fromMoney(5)
+        );
+    }
+
+    public function testInvalidNumber(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Money::fromMoney("ciao");
+    }
+
+}
+
+
+?>
