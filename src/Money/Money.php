ce0d603873d3a0ae29a415567be2c99141e25181
damn-fox <damnfox@zohomail.eu>
2020-05-07 11:24:49 +0200

    first commit

---
+++ src/Money/Money.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+final class Money
+{
+
+    private $money;
+
+    private function __construct( $money)
+    {
+        $this->ensureIsValidNumber($money);
+
+        $this->money = $money;
+    }
+
+    private function ensureIsValidNumber( $money): void
+    {
+        if (!is_int($money)) {
+            throw new InvalidArgumentException(sprintf('"%s" is not a valid money object',$money));
+        }
+    }
+
+    public static function fromMoney($money): self
+    {
+        return new self($money);
+    }
+
+}
+
+
+?>
