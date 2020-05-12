cdcd14c11f8151a1da98978964930440eb855eb2
damnfox <damnfox@damnfox.lan>
2020-05-12 15:00:04 +0200

    new branch bowling

---
+++ src/Bowling/Bowling.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Acme;
+
+final class Bowling {
+
+}
---
+++ src/Bowling/BowlingTest.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+require "vendor/autoload.php";
+
+use Acme\Bowling;
+
+use PHPUnit\Framework\TestCase;
+
+final class BowlingTest extends TestCase
+{
+    
+}
