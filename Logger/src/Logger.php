bf4a7c5cf0cf72bcf49dff89d7f980428b8f0aba
damnfox <damnfox@damnfox.lan>
2020-05-13 11:26:19 +0200

    the logs are saved in a file

--- Logger/src/Logger.php
+++ Logger/src/Logger.php
@@ -6,12 +6,26 @@ final class Logger
 {
 
     private $text = array();
+    private $pathFile = "/var/www/html/LearningPhpunit/log.txt";
+
+    public function __construct()
+    {
+        if (!file_exists($this->pathFile)) {
+            $myfile = fopen($this->pathFile, "w") or die("Unable to open file!");
+            fclose($myfile);
+        }
+
+    }
 
     public function Log(string $userString): void
     {
         if( $userString !== '')
         {
             $this->text[] = $userString;
+            
+            $current = file_get_contents($this->pathFile);
+            $current .= "$userString\n";
+            file_put_contents($this->pathFile, $current);
         }
     }
 
