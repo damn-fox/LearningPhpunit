c8e099e5e20cfaa9b804764022ea36c1636ea369
damnfox <damnfox@damnfox.lan>
2020-05-13 08:26:49 +0200

    update dir and bowling

--- Bowling/tests/BowlingTest.php
+++ Bowling/tests/BowlingTest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 require "vendor/autoload.php";
 
@@ -8,5 +10,52 @@ use PHPUnit\Framework\TestCase;
 
 final class BowlingTest extends TestCase
 {
-    
+    private $g;
+
+    public function setUp(): void
+    {
+        $this->g = new Bowling;
+    }
+
+    /**
+     * @test
+     */
+    public function rollAllOnes()
+    {
+        $this->rollMany(20, 1);
+        $this->assertEquals(20, $this->g->score());
+    }
+
+    /**
+     * @test
+     */
+    public function RollOneSpare()
+    {
+        $this->g->roll(5);
+        $this->g->roll(5);
+        $this->g->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertEquals(16, $this->g->score());
+    }
+
+    /**
+     * @test
+     **/
+    public function RollPerfectGame()
+    {
+        $this->rollMany(12, 10);
+        $this->assertEquals(300, $this->g->score());
+    }
+
+    public function rollMany($n, $pins)
+    {
+        for ($i = 0; $i < $n; $i++) {
+            $this->g->roll($pins);
+        }
+    }
+
+    public function rollStrike()
+    {
+        $this->g->roll(10);
+    }
 }
